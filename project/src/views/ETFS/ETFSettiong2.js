59cdbb7f608b95d49265676dd11cf5e9b9309211
baebyeolha <qufgk0207@naver.com>
2024-03-04 17:09:09 +0900

    ETF Setting2 컴포넌트 분리 오류 수정

--- project/src/views/ETFS/ETFSettiong2.js
+++ project/src/views/ETFS/ETFSettiong2.js
@@ -1,37 +1,35 @@
 import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { MyContext, ETFListContext } from "./ETFmaker";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardTitle,
-  Row,
-  Col,
-  Pagination,
-} from "reactstrap";
-import { Form, Button, Tab, Tabs } from "react-bootstrap";
+import { MyContext } from "./ETFmaker";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import KOSPI from "./ETF2_list";
+
+// reactstrap components
+import { Card, CardHeader, CardBody, CardTitle, Row, Col } from "reactstrap";
+import { Form, Table, Button } from "react-bootstrap";
+import { ETFListContext } from "./ETFmaker";
 import {
   fetchkospi200,
   searchkospi200,
   fetchkospi200price,
 } from "lib/api/stock";
-import KOSPI from "./ETFSetting2_list";
+import Pagination from "react-bootstrap/Pagination";
 
 export default function ETFSetting2() {
   const [ETFlist, setETFlist] = useState([]); //선택 종목
   const [SearchText, setSearchText] = useState(); //검색 키워드
-  const [list_kospi200, setList] = useState([]);
-  //불러오는 전체 리스트 (코스피200)
-  const { setContextValue } = useContext(MyContext); // 설정 페이지 이동
+  const [list3, setList3] = useState([]);
+  //불러오는 전체 리스트 (코스피, 코스피200, 코스닥)
+  const { setContextValue } = useContext(MyContext); //페이지 이동
   const { etfList, setEtfList } = useContext(ETFListContext); //post데이터 저장
   const [page, setPage] = useState(1); //현재 페이지
-  const [pages_kospi200, setPages] = useState(0);
+  const [pages3, setPages3] = useState(0);
   //총페이지
   const [start, setStart] = useState(1);
   const [end, setEnd] = useState(5);
-  //종목 페이지 이동
-  const [pagePrice, setPagePrice] = useState([]); // 종목 가격 list
+  //페이지 이동
+  const [pagePrice, setPagePrice] = useState([]);
 
   //수익률 들고오기
   async function returntrend(list) {
@@ -56,8 +54,8 @@ export default function ETFSetting2() {
     const fetchData = async () => {
       // 코스피 200 데이터 가져오기
       let response = await fetchkospi200(page); // 페이지 번호를 인자로 넘깁니다.
-      setList(response.docs);
-      setPages(response.totalPages);
+      setList3(response.docs);
+      setPages3(response.totalPages);
 
       // 선택한 페이지에 해당하는 가격 정보 가져오기
       const priceArray = await returntrend(response.docs);
@@ -68,7 +66,7 @@ export default function ETFSetting2() {
     fetchData();
   }, [page, start, end]); // 페이지 번호가 변경될 때마다 useEffect가 다시 실행되도록 의존성 배열에 page 추가
 
-  //종목 페이지 이동
+  //페이지 이동
   let items = [];
   for (let number = start; number <= end; number++) {
     items.push(
@@ -83,14 +81,12 @@ export default function ETFSetting2() {
     );
   }
 
-  //페이지별 종목 들고오기
   async function paging(id) {
     let response = await fetchkospi200(id);
-    setList(response.docs);
+    setList3(response.docs);
     setPage(id);
   }
 
-  //종목 페이지 이동 (이전, 이후)
   function pluspage() {
     let newEnd = end + 5;
     let newStart = start + 5;
@@ -107,20 +103,22 @@ export default function ETFSetting2() {
     paging(newStart);
   }
 
-  //선택 종목 리스트 만들기
+  //종목 선택
   function check(item) {
+    console.log(item.stockItem);
     setETFlist((prev) => {
       const itemExists = prev.some(
         (prevItem) => prevItem.name === item.stockItem.name
       );
       let itemCodes;
+      console.log(itemExists);
       if (itemExists) {
         itemCodes = prev.filter((prevItem) => prevItem !== item.stockItem);
       } else {
         itemCodes = [...prev, item.stockItem];
       }
 
-      // 선택된 종목 코드를 전역변수인 etfList에 저장
+      // 선택된 항목들을 etfList에 저장
       setEtfList({ ...etfList, itemCodes });
       return itemCodes;
     });
@@ -129,7 +127,7 @@ export default function ETFSetting2() {
   //종목검색
   async function K(id, text) {
     let response = await searchkospi200(id, text);
-    setList(response.docs);
+    setList3(response.docs);
   }
 
   return (
@@ -164,7 +162,7 @@ export default function ETFSetting2() {
                     </Button>
                   </div>
                   <div style={{ marginTop: "20px" }}>ETF 상품</div>
-                  {/* 코스피 200종목 보여주기 */}
+                  {/* 종목 보여주기 */}
                   <Tabs
                     defaultActiveKey="profile"
                     id="uncontrolled-tab-example"
@@ -172,41 +170,42 @@ export default function ETFSetting2() {
                   >
                     <Tab eventKey="kospi200" title="코스피200">
                       <KOSPI
-                        list_kospi200={list_kospi200}
+                        list_kospi200={list3}
                         pagePrice={pagePrice}
                         ETFlist={ETFlist}
                         check={check}
                       />
-                      <div
-                        style={{
-                          display: "flex",
-                          justifyContent: "center",
-                          gap: "2%",
-                          alignItems: "center",
-                        }}
-                      >
-                        {end === 5 ? null : (
-                          <Button
-                            onClick={() => {
-                              minuspage();
-                            }}
-                          >
-                            이전
-                          </Button>
-                        )}
-                        <Pagination size="sm">{items}</Pagination>
-                        {end > pages_kospi200 ? null : (
-                          <Button
-                            onClick={() => {
-                              pluspage();
-                            }}
-                          >
-                            이후
-                          </Button>
-                        )}
-                      </div>
                     </Tab>
                   </Tabs>
+
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      gap: "2%",
+                      alignItems: "center",
+                    }}
+                  >
+                    {end === 5 ? null : (
+                      <Button
+                        onClick={() => {
+                          minuspage();
+                        }}
+                      >
+                        이전
+                      </Button>
+                    )}
+                    <Pagination size="sm">{items}</Pagination>
+                    {end > pages3 ? null : (
+                      <Button
+                        onClick={() => {
+                          pluspage();
+                        }}
+                      >
+                        이후
+                      </Button>
+                    )}
+                  </div>
 
                   {/* 선택종목 보여주기 */}
                   <div style={{ display: "flex", marginTop: "3%" }}>
@@ -244,11 +243,11 @@ export default function ETFSetting2() {
                     <Link>
                       <Button
                         onClick={() => {
-                          if (!etfList.itemCodes) {
-                            alert("종목을 선택하지 않았습니다");
-                          } else {
-                            setContextValue("3");
-                          }
+                          // if (!etfList.itemCodes) {
+                          //   alert("종목을 선택하지 않았습니다");
+                          // } else {
+                          //   setContextValue("3");
+                          // }
                           setContextValue("3");
                         }}
                         className="d-flex flex-column justify-content-center align-items-end"
--- project/src/views/ETFS/ETFmaker.js
+++ project/src/views/ETFS/ETFmaker.js
@@ -6,17 +6,21 @@ import ETFSetting3 from "./ETFSetting3";
 export const MyContext = React.createContext();
 export const ETFListContext = React.createContext();
 
+export default function ETFMaker() {
+  const [contextValue, setContextValue] = useState("1");
+  const [etfList, setEtfList] = useState(); //시작일, 종료일, 투자금액, 투자종목, 투자비중 저장
 
-export default function ETFMaker(){
-    const [contextValue, setContextValue] = useState('1');
-    const [etfList, setEtfList] = useState();  //시작일, 종료일, 투자금액, 투자종목, 투자비중 저장
-
-
-    return (
-        <MyContext.Provider value={{ contextValue, setContextValue }}>
-            <ETFListContext.Provider value={{ etfList, setEtfList }}>
-            {contextValue === '1' ? <ETFSetting /> : contextValue === '2' ? <ETFSetting2 /> : <ETFSetting3 />}
-            </ETFListContext.Provider>
-        </MyContext.Provider>
-    );
+  return (
+    <MyContext.Provider value={{ contextValue, setContextValue }}>
+      <ETFListContext.Provider value={{ etfList, setEtfList }}>
+        {contextValue === "1" ? (
+          <ETFSetting />
+        ) : contextValue === "2" ? (
+          <ETFSetting2 />
+        ) : (
+          <ETFSetting3 />
+        )}
+      </ETFListContext.Provider>
+    </MyContext.Provider>
+  );
 }
